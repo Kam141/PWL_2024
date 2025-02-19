5dcc531d798c23cbe94d83a2a31e8e24fdfded83
Kam141 <kamilafadhil@gmail.com>
2025-02-19 09:06:35 +0700

    praktikum route parameters

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,19 @@ Route::get('/', function ()  {
 Route::get('/about', function ()  {
     return '2341720175_Kamila habiba Putri Ananta';
 });
+
+//praktikum route parameters
+//a
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+//d
+Route::get('/post/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-".$commentId;
+});
+
+//f
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
